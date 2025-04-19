0456daf10aafd17157f0da1b7eb466575b2f714e
Miles Georgi <azimux@gmail.com>
2025-04-19 14:12:42 -0700

    Create ruby project files

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [Unreleased]
+
+- Move version.rb.erb to top-level
+
+## [0.0.1] - 2025-04-19
+
+- Initial release
---
+++ spec/foobara/auth_http_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::AuthHttp do
+  it "has a version number" do
+    expect(Foobara::AuthHttp::VERSION).to_not be_nil
+  end
+end
